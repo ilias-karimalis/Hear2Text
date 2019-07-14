d21df012059c877a50382ddb54fa622b20b510c3
Allen Li <headhuntar@gmail.com>
2019-07-14 01:15:54 -0700

    finished hard stuff

--- index.js
+++ index.js
@@ -1,15 +1,91 @@
-const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
+const record = require('node-record-lpcm16');
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const projectId = 'angelhack2019-1563002455055'
+const keyFilename = './angelhack2019-804a5c7d40ca.json'
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/landing'))
-  .get('/demo', (req, res) => {
-    res.render('pages/demo');
+// Creates a client
+const client = new speech.SpeechClient({ projectId, keyFilename });
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data => {
+    io.emit('chat message', data.results[0].alternatives[0].transcript);
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : `\n\nReached transcription time limit, press Ctrl+C\n`
+    )
+  }
+  );
+
+// Start recording and send the microphone input to the Speech API
+record
+  .start({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: 'rec', // Try also "arecord" or "sox"
+    silence: '10.0',
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .on('error', console.error)
+  .pipe(recognizeStream);
+
+console.log('Listening, press Ctrl+C to stop.');
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('pages/landing')
+})
+app.get('/demo', (req, res) => {
+  res.render('pages/demo')
+})
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is up on PORT ${PORT}!`);
+});
+
